632dde24fcaaa61e1c70eb3ee8077fe5103c890f
Hassan Andrabi <handrabi53@gmail.com>
2022-11-08 02:02:58 +1100

    Generate MSE per class.

--- test.py
+++ test.py
@@ -1,9 +1,6 @@
-import numpy as np
+l1 = [2, 4, 5, 6]
+l2 = [1, 2, 2, 3]
 
-entropy1 = (9/14)*np.log2(14/9) + (5/14)*np.log2(14/5)
-entropy2 = (4/14)*((2/4)*np.log2(4/2) + (2/4)*np.log2(4/2)) + (6/14)*((4/6)*np.log2(6/4) + (2/6)*np.log2(6/2)) + (4/14)*((3/4)*np.log2(4/3) + (1/4)*np.log2(4/1))
-IG = entropy1 - entropy2
-SI = - ( (4/14)*np.log2(4/14) + (6/14)*np.log2(6/14) + (4/14)*np.log2(4/14) )
-print(SI)
-print(IG/SI)
-print(IG)
+l3 = [instance[0] for instance in zip(l1, l2) if instance[1] == 2]
+print(l3)
+print(l2.index(2))
